90c98bc41d1c0a8c1d6532c02edf21c73bd84026
MrGeislinger <9027783+MrGeislinger@users.noreply.github.com>
2025-02-15 22:31:04 -0800

    Basic BlueSky functionality on posts (using ATProto)

---
+++ bluesky.py
@@ -0,0 +1,108 @@
+import atproto
+from dataclasses import dataclass
+import dotenv
+import os
+
+@dataclass
+class Post:
+    did: str
+    tid: str
+    url: str | None = None
+
+
+class BlueSky:
+    '''
+    '''
+
+    def __init__(
+        self,
+        verbose: bool = False,
+    ):
+        self._verbose: bool = verbose
+        self.client: atproto.Client | None = None
+    
+    @property
+    def verbose(self):
+        return self._verbose
+
+    @verbose.setter
+    def verbose(self, value):
+        self._verbose = value 
+
+    def login_client(self):
+        dotenv.load_dotenv('.env')
+        username = os.getenv('BSKY_USERNAME')
+        password = os.getenv('BSKY_PASSWORD')
+        if self.verbose:
+            print(f'{username=}')
+        self.client = atproto.Client()
+    
+    def is_did(
+        self,
+        did_candidate: str,
+    ) -> bool:
+        '''Determine if a given string is a valid DID (repository)'''
+        # TODO: Actual validation
+        return did_candidate[:3] == 'did'
+
+    def resolve_post(
+        self,
+        url: str,
+    ) -> Post:
+        '''Get from URL 
+
+        Returns:
+          DID (repository)
+          TID (record key)
+        '''
+        # Split up URL
+        # https://bsky.app/profile/{USER}/post/{POST}
+        url_parts = (
+            url
+            .replace('https://', '')
+            .replace('http://', '')
+            .split('/')
+        )
+        # TODO: check if did string
+        did = url_parts[2]
+        tid = url_parts[4]
+        return Post(did=did, tid=tid, url=url)
+
+
+    def get_uri(
+        self,
+        user_did,
+        post_rkey,
+    ) -> str:
+        '''Get at://... URI'''
+        quote_post_uri = f'at://{user_did}/app.bsky.feed.post/{post_rkey}'
+        return quote_post_uri
+        
+
+    # handle_response = client.resolve_handle(user_to_quote)
+    def get_post(
+        self,
+        post_url: str | None = None,
+        user: str | None = None,
+        user_did: str | None = None,
+        post_rkey: str | None = None,
+    ) -> atproto.models.AppBskyFeedPost.GetRecordResponse:
+        '''Define (post_url) OR ( (user OR user_did) AND post_rkey )
+        '''
+        # TODO: Ensure at least one is defined.
+        if post_url:
+            post = self.resolve_post(post_url)
+        else:
+            user_did = (
+                user_did if user_did
+                else self.client.resolve_handle(user).did
+            )
+            post = Post(did=user_did, tid=post_rkey)
+
+        
+        quoted_post = self.client.get_post(
+            profile_identify=post.did,
+            post_rkey=post.tid,
+        )
+        return quoted_post
+        
